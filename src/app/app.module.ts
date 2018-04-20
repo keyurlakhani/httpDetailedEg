5750ebb79db2c0d60eaeeb94337e4abd1334379d
keyurlakhani <keyur.lakhani99@gmail.com>
2018-04-19 17:12:20 -0700

    HTTP Detailed Example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
+import { AppErrorHandler } from './common/app-error-handler';
+import { PostService } from './services/post.service';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '' , component: PostsComponent }
+    ])
   ],
-  providers: [],
+  providers: [
+    PostService,
+    { provide: ErrorHandler, useClass: AppErrorHandler }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
